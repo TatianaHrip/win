5581b493045db73ac0dcefc50a55a40ce6dc70fb
Tatyana Hripkova <tkhripkova@bk.ru>
2020-11-20 23:54:31 +0200

    militc commit

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         float tiket = 4_300f;
         float miles = 20f;
-        float bonus = tiket/miles;
+        float bonus = tiket / miles;
         System.out.println(bonus);
     }
 }
