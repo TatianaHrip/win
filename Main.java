c5aff70fcab3eefb76c7c2956bc981fde41c1dfb
Tatyana Hripkova <tkhripkova@bk.ru>
2020-11-20 20:37:58 +0200

    miles commit

--- Main.java
+++ Main.java
@@ -1,2 +1,9 @@
 public class Main {
+
+    public static void main(String[] args) {
+        float tiket = 4_300f;
+        float miles = 20f;
+        float bonus = tiket+miles;
+        System.out.println(bonus);
+    }
 }
